bd420b23ed44a6417cb380f99950dc5cabbf3dc7
txt_xd <l9596874@gmail.com>
2026-04-30 19:39:36 +0500

    bugfix genres

--- resources/genres_resources.py
+++ resources/genres_resources.py
@@ -7,45 +7,42 @@ from .auth import check_api_key
 
 
 def not_found_genre(genre_id):
-    db_sess = db_session.create_session()
-    genre = db_sess.query(Genre).get(genre_id)
-    db_sess.close()
-    if not genre:
-        abort(404, message=f'Genre {genre_id} not found')
-    return db_sess, genre
+    with db_session.create_session() as db_sess:
+        genre = db_sess.query(Genre).get(genre_id)
+        if not genre:
+            abort(404, message=f'Genre {genre_id} not found')
+        return genre
 
 
 class GenreResource(Resource):
     def get(self, genre_id):
         check_api_key()
-        db_sess, genre = not_found_genre(genre_id)
-        db_sess.close()
+        genre = not_found_genre(genre_id)
         return jsonify({'genre': genre.to_dict()})
 
     def delete(self, genre_id):
         check_api_key()
-        db_sess, genre = not_found_genre(genre_id)
-        db_sess.delete(genre)
-        db_sess.commit()
-        db_sess.close()
-        return jsonify({'success': 'OK'})
+        with db_session.create_session() as db_sess:
+            genre = db_sess.query(Genre).get(genre_id)
+            if not genre:
+                abort(404, message=f'Genre {genre_id} not found')
+            db_sess.delete(genre)
+            db_sess.commit()
+            return jsonify({'success': 'OK'})
 
 
 class GenreListResource(Resource):
     def get(self):
         check_api_key()
-        db_sess = db_session.create_session()
-        genres = db_sess.query(Genre).all()
-        db_sess.close()
-        return jsonify({'genres': [g.to_dict() for g in genres]})
+        with db_session.create_session() as db_sess:
+            genres = db_sess.query(Genre).all()
+            return jsonify({'genres': [g.to_dict() for g in genres]})
 
     def post(self):
         check_api_key()
         args = genre_parser.parse_args()
-        db_sess = db_session.create_session()
-
-        genre = Genre(title=args['title'])
-        db_sess.add(genre)
-        db_sess.commit()
-        db_sess.close()
-        return jsonify({'id': genre.id})
+        with db_session.create_session() as db_sess:
+            genre = Genre(title=args['title'])
+            db_sess.add(genre)
+            db_sess.commit()
+            return jsonify({'id': genre.id})
